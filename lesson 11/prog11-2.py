11f9960256635eaecd148865ec4c1c8868e2a7c8
dmialder <55893537+dmialder@users.noreply.github.com>
2022-11-20 15:36:01 +0300

    Add files via upload

---
+++ lesson 11/prog11-2.py
@@ -0,0 +1,37 @@
+import os, re
+import threading
+
+
+def thread_job(suffix):
+    with lock:
+        global received_packages
+        global status
+        ip = "192.168.178." + str(suffix)
+        ping_out = os.popen("ping -q -c2 " + ip, "r")  # получение вердикта
+        print("... pinging ", ip)
+        while True:
+            line = ping_out.readline()
+            if not line:
+                break
+            n_received = received_packages.findall(line)
+            if n_received:
+                print(ip + ": " + status[int(n_received[0])])
+
+
+def run_threads():
+    threads = [
+        threading.Thread(target=thread_job, args=(i,))
+        for i in range(20, 30)
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+lock = threading.Lock()
+received_packages = re.compile(r"(\d) received")
+status = ("no response", "alive but losses", "alive")
+
+run_threads()
+print("finish")
